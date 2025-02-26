ab049ded8674214cd577f80fd8488ba601b27d0a
Nguyen The Quoc <quoc9193@gmail.com>
2025-02-26 17:17:50 +0700

    class-style

---
+++ 05-class-style/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, reactive } from "vue";
+const color = ref("blue");
+const background = ref("yellow");
+const fontSize = ref(30);
+
+const styleObject = reactive({
+  border: "none",
+  borderRadius: "6px",
+});
+</script>
+
+<template>
+  <div>
+    <button
+      :style="[
+        {
+          color,
+          background,
+          fontSize: fontSize + 'px',
+        },
+        styleObject,
+      ]"
+    >
+      Button
+    </button>
+  </div>
+</template>
