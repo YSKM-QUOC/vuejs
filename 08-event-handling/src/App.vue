ab273d6bb111240af84b64aa774f402ab35f059a
Nguyen The Quoc <quoc9193@gmail.com>
2025-02-28 16:48:48 +0700

    event handling

---
+++ 08-event-handling/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+
+const submit = (event) => {
+  if (event.type === "keyup" && event.altKey && event.key === "Enter") {
+    alert("Xin chào");
+  } else if (event.type === "click" && event.shiftKey) {
+    alert("Xin chào");
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h1>Event handling</h1>
+    <!-- Bắt sự kiện Alt + Enter -->
+    <input @keyup="submit" placeholder="Nhập tên của bạn" />
+
+    <!-- Bắt sự kiện Shift + Click -->
+    <button @click="submit">Submit</button>
+  </div>
+</template>
